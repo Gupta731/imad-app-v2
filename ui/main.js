0fb1b22c3795f0bd14b913a52a9fc4110f8b6f1b
Sourabh Gupta <sourabh.gupta.731@gmail.com>
2017-02-04 18:14:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,12 @@ var element = document.getElementById('main-text'
 element.innerHTML = 'New Value';
 //move the image
 var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
 img.onClick=function(){
-    img.style.marginLeft='150px';
+    var interval = setInterval(moveRight, 100);
+   
 };
