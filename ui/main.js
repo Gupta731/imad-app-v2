f12aba4d74882f7ebef00f66d8a5df15ca522d11
Sourabh Gupta <sourabh.gupta.731@gmail.com>
2017-02-05 19:16:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 //counter code
 var button = document.getElementById('counter');
 var counter=0;
-button.oneClick = function(){
+button.onClick = function(){
     counter = counter + 1;
     var span = document.getElementById('count');
     span.innerHTML=counter.toString();
