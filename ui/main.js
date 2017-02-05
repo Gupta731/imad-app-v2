784189c8da11c52d010b8b5291e0946665297434
Sourabh Gupta <sourabh.gupta.731@gmail.com>
2017-02-05 19:56:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,3 +18,16 @@ button.onClick = function(){
    request.open('GET',"http://cloud.imad.hasura-app.io/counter", true);
    request.send(null);
 };
+//submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onClick = function(){
+    var names = ['name1', 'name2', 'name3', 'name4'];
+    var list = '';
+    for(var i=0;i<names.length;i++){
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('nameList');
+    ul.innerHTML=list;
+};
