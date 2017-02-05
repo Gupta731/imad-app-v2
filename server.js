e4d0b8af427f42d985bb9abe2c35aeb71d738e9d
Sourabh Gupta <sourabh.gupta.731@gmail.com>
2017-02-05 20:15:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,6 +15,14 @@ app.get('/counter', function(req, res){
     res.send(counter.toString());
     
 });
+var names = [];
+app.get('/submit-name/name', function(req, res){
+    var name=req.query.name; 
+    names.push(name);
+    res.send(JSON.stringify(names));
+    
+});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
@@ -41,13 +49,6 @@ app.get('/article-three', function(req,res)
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-var names = [];
-app.get('/submit-name/name', function(req, res){
-    var name=req.query.name; 
-    names.push(name);
-    res.send(JSON.stringify(names));
-    
 });
 
 
