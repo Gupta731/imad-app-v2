39c16d2adb0525c0d2758a47c1f471cbbeb553e5
Sourabh Gupta <sourabh.gupta.731@gmail.com>
2017-02-05 19:28:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,20 @@
 //counter code
 var button = document.getElementById('counter');
-var counter=0;
+
 button.onClick = function(){
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML=counter.toString();
-    
+    var request = new XMLHttpRequest();
+    request.onreadystateChange=function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            //Take some action
+            if(requst.status===200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+        
+    };
+   //make a request
+   request.open('GET',"http://cloud.imad.hasura-app.io/counter", true);
+   request.send(null);
 };
