589d42e4859e889dfe3b9ed411b0d3725e70cc42
Sourabh Gupta <sourabh.gupta.731@gmail.com>
2017-02-26 20:42:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+var config = {
+  host: 'db.imad.hasura-app.io',
+  user: gupta731,
+  port:'5432',
+  database: 'gupta731',
+  password: process.emv.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -8,7 +16,19 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+var pool = new Pool(config)
+app.get('/test-db',function(req, res){
+    pool.query('SELECT * from test', function(err, result) {
+      
+      if(err) {
+      res.status(500).send(err.toString());
+      }
+      else
+      {
+          res.send(JSON.stringify(result));
+          }
+          });
+      });
 var counter=0;
 app.get('/counter', function(req, res){
     counter=counter+1;
